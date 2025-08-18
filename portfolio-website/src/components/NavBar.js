55e9928c189a6e6a9d2138ec131b6ae15dbb9ad0
CarterKreis <cckreis0507@gmail.com>
2025-08-18 17:00:38 -0400

    it runs?

--- portfolio-website/src/components/NavBar.js
+++ portfolio-website/src/components/NavBar.js
@@ -1,13 +1,13 @@
-import {useState } from 'react';
-import {Navbar, Container} from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';
 
 export const NavBar = () => {
-    const {activeLink, setActiveLink} = useState('home');
-    const {scrolled, setScrolled} = useState(false);
+    const [activeLink, setActiveLink] = useState('home');
+    const [scrolled, setScrolled] = useState(false);
 
     useEffect(() => {
         // Function to handle scroll event
